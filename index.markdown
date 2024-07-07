7e213f5403ec3c0990f2d949ab5c579707fe38f1
DFelipehDEV <thediogopcm@gmail.com>
2024-07-07 13:53:58 +0100

    rato

---
+++ index.markdown
@@ -0,0 +1,28 @@
+---
+
+layout: home
+
+---
+# Zasus
+<div class="bg-slate-100 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
+  <div>
+    <span class="inline-flex items-center justify-center p-1 bg-primary rounded-md shadow-lg" markdown=1 >
+      <img class=" h-48 w-48" src="img/mug/zasus-500.webp">
+    </span>
+
+<div markdown="1"> 
+
+# **Contact**
+
+| **Twitter:**       | [@MFilipeDiogo](https://x.com/MFilipeDiogo) |
+| **Github:**        | [DFelipehDEV](https://github.com/DFelipehDEV)  |
+| **Email:**         | [zasus.contact@gmail.com](mailto:zasus.contact@gmail.com) | 
+
+</div>
+  </div>
+  <span style="color:blue">some *blue* text</span>.
+  <h3 class="text-secondary mt-5 text-base font-medium tracking-ti353A45ght">About</h3>
+  <div markdown="1">
+The Zero Gravity Pen can be used to write in any orientation, including upside-down. It even works in outer space.
+  </div>
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,51 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    '_includes/**/*.html',
+    '_layouts/**/*.html',
+    '*.markdown'
+  ],
+  theme: {
+    colors: {
+      primary: '#ff2c6d', // pink
+      secondary: '#D8B64D', // yellow
+      light: '#EAD6F0',
+      white: '#FFFFFF',
+      slate: {
+        50: "#202427",
+        100: "#232830",
+        200: "#2B303B",
+        300: "#353A45",
+      },
+      DEFAULT: '#232830',
+    },
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme('colors.gray.700'),
+            h2: {
+              color: theme('colors.secondary'),
+            },
+            h3: {
+              color: theme('colors.gray.800'),
+            },
+            strong: {
+              color: theme('colors.gray.800'),
+            },
+            a: {
+              color: theme('colors.green.500'),
+              '&:hover': {
+                color: theme('colors.green.600')
+              },
+            },
+          },
+        },
+      })
+    },
+  },
+  
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
